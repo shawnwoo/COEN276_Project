7d855a5c979ecc7c5a7da5f8a29876a27288bf74
Hayley Wu <hwu@dhcp138.home>
2012-11-25 00:39:16 -0800

    query last insert number and return to alert box after order

--- scripts/placeorder.php
+++ scripts/placeorder.php
@@ -7,14 +7,21 @@
     require "connect.php";
     
     mysql_query("insert into orderNumbers (order_num) values(NULL)");
-    // query the order number and put it in variable to return to js callback
-    $orderNumber = 123; //for now
+    
+    // get the order number to return to user
+    $result = mysql_query("select last_insert_id()");
+    if($row = mysql_fetch_array($result))
+    {
+        $orderNumber = $row['last_insert_id()'];
+    }
     
     foreach($_POST as $key => $value)
     {
 
         $sql = "insert into orders (order_num, date, userId, item_name, qty) values (last_insert_id(), now(), $userId, '$key', $value)";
         mysql_query($sql);
+
+        
     }
     
     echo $orderNumber;
